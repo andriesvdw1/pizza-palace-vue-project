836e162503a30e0a351010b54335ee684c01103b
Andries <andriesvdw1@gmail.com>
2026-07-03 12:17:42 +0200

    Add image files to src assets images folder

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import MenuView from '../views/MenuView.vue'
+import AboutView from '../views/AboutView.vue'
+import AdminView from '../views/AdminView.vue'
+
+const router= createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/menu',
+      name: 'menu',
+      component: MenuView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: AdminView
+    }
+  ]
+})
+
+export default router
